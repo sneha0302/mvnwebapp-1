baa720466e6c873f614b6691621028df6508e6eb
sneha0302 <77618131+sneha0302@users.noreply.github.com>
2021-12-10 11:32:30 +0530

    Update Butler.java

--- Butler.java
+++ Butler.java
@@ -10,7 +10,7 @@ public class Butler {
         ArrayList<String> list1 = new ArrayList<String>();
         list1.add("Hello");
         ArrayList<String> list2 = new ArrayList<String>();
-        list2.add("World");
+        list2.add("World!!");
 
         System.out.println(ListUtils.union(list1, list2));
     }
